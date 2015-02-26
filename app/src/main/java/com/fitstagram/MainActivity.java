9fd9b48d454bfd8bb9cf4115bc8a4aa59d9bf920
Sean Gerard Lee <leesg@tcd.ie>
2015-02-26 16:11:18 +0000

    test2

--- app/src/main/java/com/fitstagram/MainActivity.java
+++ app/src/main/java/com/fitstagram/MainActivity.java
@@ -1,39 +1,43 @@
 package com.fitstagram;
 
-import android.location.GpsStatus;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View.OnClickListener;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.content.Intent;
+import android.provider.MediaStore;
 
-public class MainActivity extends ActionBarActivity{
+public class MainActivity extends ActionBarActivity {
 
-    OnClickListener l1;
-    Button b1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView takenPictureView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        b1 = (Button) findViewById(R.id.button);
-    //    tv1 = (TextView) findViewById(R.id.textView1);
-
-        b1.setOnClickListener(l1);
-
-    }
-
-    public void onClick(View v)
-    {
-        // TODO Auto-generated method stub
-        Intent i = new Intent(getApplicationContext(),TakePicture.class);
-        startActivity(i);
         setContentView(R.layout.activity_take_picture);
+
+        Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+        takenPictureView = (ImageView) findViewById(R.id.takenPictureView);
     }
 
+    public void launchCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Take a picture and pass results along to onActivityResult
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
 
+    //if you want to return the image taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // checks that there arent errors or anything
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            //get the photo
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            takenPictureView.setImageBitmap(photo);
+        }
+    }
 }
