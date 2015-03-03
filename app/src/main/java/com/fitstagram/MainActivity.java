92823a343156343c974ef06b742542173c9b754c
laurmurclar <murphl26@tcd.ie>
2015-03-03 12:10:37 +0000

    Adding Sean's stuff to a different branch

--- app/src/main/java/com/fitstagram/MainActivity.java
+++ app/src/main/java/com/fitstagram/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.graphics.Bitmap;
 import android.content.Intent;
 import android.provider.MediaStore;
@@ -12,20 +15,16 @@ import android.provider.MediaStore;
 public class MainActivity extends ActionBarActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView takenPictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+     //   ActionBar actionBar = getSupportActionBar();
+     //   actionBar.show();
         setContentView(R.layout.activity_main);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    //    Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+    //    takenPictureView = (ImageView) findViewById(R.id.takenPictureView);
     }
 
     public void launchCamera(View view){
@@ -33,6 +32,35 @@ public class MainActivity extends ActionBarActivity {
         // Take a picture and pass results along to onActivityResult
         startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_home:
+                setContentView(R.layout.activity_main);
+                return true;
+            case R.id.takePictureButton:
+                setContentView(R.layout.activity_take_picture);
+                Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+                takenPictureView = ((ImageView) findViewById(R.id.takenPictureView));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
 
     //if you want to return the image taken
     @Override
