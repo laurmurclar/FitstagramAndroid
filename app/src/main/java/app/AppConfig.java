50c2bb0336da13d873334f1d30156a4f398bf0f1
Sean Gerard Lee <leesg@tcd.ie>
2015-03-24 23:46:20 +0000

    Login and registration works now

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,10 @@
+package app;
+/**
+ * Created by Sean on 23/03/2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://killiand.netsoc.ie/progproj/php/";
+    // Server user register url
+    public static String URL_REGISTER = "http://killiand.netsoc.ie/progproj/php/";
+}
