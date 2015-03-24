3d864edaf3fa117cbe96bd08d190ad6d4f13e172
Sean Gerard Lee <leesg@tcd.ie>
2015-03-24 23:01:13 +0000

    The user login and registration is really close to being done i think. I got a but further than i was earlier. It looks like it actually tries to register and sign in when you click the buttons but then doesnt do anything.

--- app/src/main/java/com/fitstagram/MainActivity.java
+++ app/src/main/java/com/fitstagram/MainActivity.java
@@ -8,18 +8,73 @@ import android.view.View;
 import android.graphics.Bitmap;
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import helper.SQLiteHandler;
+import helper.SessionManager;
 
 public class MainActivity extends ActionBarActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+    private SQLiteHandler db;
+    private SessionManager session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);    // changed so that it goes straight to login screen
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
 
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
     }
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        session.setLogin(false);
 
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
